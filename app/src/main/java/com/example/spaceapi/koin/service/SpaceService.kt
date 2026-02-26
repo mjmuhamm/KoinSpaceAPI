e8588cf318114af54b055a74345be86daee38927
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2026-02-26 08:48:02 -0500

    consolidation

--- app/src/main/java/com/example/spaceapi/koin/service/SpaceService.kt
+++ app/src/main/java/com/example/spaceapi/koin/service/SpaceService.kt
@@ -16,17 +16,8 @@ interface SpaceService {
 
 class SpaceServiceImpl(private val spaceRepository: SpaceRepository) : SpaceService {
 
+    override suspend fun loadResults(): Result<List<com.example.spaceapi.model.firstPage.Result>> = spaceRepository.getInfo()
+
     override suspend fun getResult(id: String): Result<SecondResponse> = spaceRepository.findResult(id)
 
-    override suspend fun loadResults(): Result<List<com.example.spaceapi.model.firstPage.Result>> =
-        withContext(Dispatchers.IO) {
-            try {
-                val response = RetrofitClient.api.getInfo()
-                val info = response.results
-                Result.success(info)
-//                spaceRepository.getInfo()
-            } catch (e: Exception) {
-                Result.failure(e)
-            }
-        }
 }
